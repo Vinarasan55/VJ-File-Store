a19cb5beb5f932023a918cb548580f5797ba347f
Vinarasan55 <122718267+Vinarasan55@users.noreply.github.com>
2024-08-24 22:53:10 +0530

    Update commands.py

--- clone_plugins/commands.py
+++ clone_plugins/commands.py
@@ -52,7 +52,7 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('💝 ᴡʜᴀᴛ's ᴀᴘᴘ ɢʀᴏᴜᴘ ', url='https://chat.whatsapp.com/Jm79XuRDvxb1Zl1oVL9OTK')
+            InlineKeyboardButton('💝 ᴡʜᴀᴛs ᴀᴘᴘ ɢʀᴏᴜᴘ ', url='https://chat.whatsapp.com/Jm79XuRDvxb1Zl1oVL9OTK')
             ],[
             InlineKeyboardButton('🤖 ᴄʀᴇᴀᴛᴇ ʏᴏᴜʀ ᴏᴡɴ ᴄʟᴏɴᴇ ʙᴏᴛ', url=f'https://t.me/{BOT_USERNAME}?start=clone')
             ],[
@@ -180,7 +180,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.message.delete()
     elif query.data == "start":
         buttons = [[
-            InlineKeyboardButton('💝 ᴡʜᴀᴛ's ᴀᴘᴘ ɢʀᴏᴜᴘ ', url='https://chat.whatsapp.com/Jm79XuRDvxb1Zl1oVL9OTK')
+            InlineKeyboardButton('💝 ᴡʜᴀᴛs ᴀᴘᴘ ɢʀᴏᴜᴘ ', url='https://chat.whatsapp.com/Jm79XuRDvxb1Zl1oVL9OTK')
             ],[
             InlineKeyboardButton('🤖 ᴄʀᴇᴀᴛᴇ ʏᴏᴜʀ ᴏᴡɴ ᴄʟᴏɴᴇ ʙᴏᴛ', url=f'https://t.me/{BOT_USERNAME}?start=clone')
             ],[
